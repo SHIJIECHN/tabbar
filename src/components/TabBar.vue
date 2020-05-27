ae7baf594eafbd7a1d40497eb07e32448855b0b7
shijiechn <shijiechn@aliyun.com>
2020-05-27 20:03:55 +0800

    add tabbar

---
+++ src/components/TabBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="tabbar-item">首页</div>
+    <div class="tabbar-item">分类</div>
+    <div class="tabbar-item">购物车</div>
+    <div>我的</div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "TabBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
